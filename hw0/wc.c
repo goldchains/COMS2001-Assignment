ad29a14cd798e39b68a4c1c026bd6babca30b953
goldchains <1146939@students.wits.ac.za>
2016-07-29 11:20:22 +0200

    Add files via upload

--- hw0/wc.c
+++ hw0/wc.c
@@ -5,12 +5,37 @@
 
 void wc ( FILE *infile, char *inname);
 
-int
-main (int argc, char *argv[]) {
-    
+int main (int argc, char *argv[]) {
+   int wordcount =  0;
+   int i;
+   for(i = 1;i < argc; i++){
+     FILE *fp;
+     fp = fopen(argv[i],"r");
+     wc(fp,argv[i]);
+    fclose(fp);
+   }
+
 }
 
-void
-wc( FILE *infile, char *inname) {
-    
+void wc( FILE *infile, char *inname) {
+     char line[256];
+     int i;
+     int wordcount = 0;
+    if(read_line(infile, line, sizeof line)){
+    for(i=0;line[i] != '\0';i++){
+      if(line[i]==' ' && line[i+1] != ' ')
+         wordcount++;
+    }
+      printf("number of words: %d \n", wordcount);}
+    else
+      printf("read error\n");
+
+  /*   do{
+        ch = getc(infile);
+        printf("%c",ch);
+     }while(ch != EOF);*/
+   }
+int read_line(FILE *in, char *buffer, size_t max)
+{
+  return fgets(buffer, max, in) == buffer;
 }
